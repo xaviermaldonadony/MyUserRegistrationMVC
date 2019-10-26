569a0bfb6464bd8df06331576ed0f2b666040b55
xaviermaldonadony <xaviermaldonadony@hotmail.com>
2019-10-26 18:07:33 -0400

    commented out login repo/service

--- src/com/profile/repository/LoginRepository.java
+++ src/com/profile/repository/LoginRepository.java
@@ -1,5 +1,14 @@
-//package com.profile.repository;
-//
-//public interface LoginRepositor {
-//
-//}
+package com.profile.repository;
+import com.profile.model.Login;
+
+import java.util.*;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoginRepository extends CrudRepository<Login, Long> {
+
+  Login findByEmail(String email);
+  List<Login> findLoginBydateOfBirth(Date dateOfBirth);
+}
